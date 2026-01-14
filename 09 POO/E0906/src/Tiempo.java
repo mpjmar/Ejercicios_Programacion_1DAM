7523777ff488ac69b7d7ec2c7a520f7ad01ff272
m. paz jimenez <meripeich@gmail.com>
2026-01-14 20:07:50 +0100

    E0906 improved

--- 09 POO/E0906/src/Tiempo.java
+++ 09 POO/E0906/src/Tiempo.java
@@ -2,30 +2,40 @@ public class Tiempo {
 	
 	private int seg;
 
-	public Tiempo(int horas, int min, int seg) {
-		this.seg = horas * 3600 + min * 60 + seg;
+	public Tiempo() {
+		this.seg = 0;
 	}
 
 	public Tiempo (int seg) {
 		this.seg = seg;
 	}
 
-	public int getSeg(Tiempo t) {
-		return this.seg;
+	public Tiempo(int horas, int min, int seg) {
+		this.seg = horas * 3600 + min * 60 + seg;
+	}
+
+	public int getHoras() {
+		return seg / 3600;
+	}
+
+	public int getMin() {
+		return (seg % 3600) / 60;
+	}
+
+	public int getSeg() {
+		return seg % 60;
 	}
 
 	public Tiempo suma(Tiempo t) {
-		return new Tiempo(this.seg + t.getSeg(t));
+		return new Tiempo(this.seg + t.getSeg());
 	}
 
 	public Tiempo resta(Tiempo t) {
-		return new Tiempo(this.seg - t.getSeg(t));
+		return new Tiempo(this.seg - t.getSeg());
 	}
 
 	@Override
 	public String toString() {
-		int horas = this.seg / 3600;
-		int min = (this.seg - horas * 3600) / 60;
-		return String.format("%2dh:%2dm:%2ds", horas, min, seg % 60);
+		return String.format("%2dh:%2dm:%2ds", getHoras(), getMin(), getSeg());
 	}
 }
