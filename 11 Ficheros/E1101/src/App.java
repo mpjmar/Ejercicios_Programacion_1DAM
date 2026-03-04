4004ac7caed79f193942ea8d5ba8047c9787140f
m. paz jimenez <meripeich@gmail.com>
2026-03-04 10:54:29 +0100

    E1101

---
+++ 11 Ficheros/E1101/src/App.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter("primos.dat"));
+
+			for (int i = 1; i <= 500; i++)
+				if (esPrimo(i))
+					bw.write(i + " ");
+			bw.close();
+		} catch (IOException ioe) {
+			System.out.println("No se ha podido escribir en el fichero.");
+		}
+	}
+
+	public static boolean esPrimo(int n) {
+		
+		if (n == 1 | n == 2)
+			return false;
+
+		for (int i = 2; i <= n / 2; i++)
+			if (n % i == 0)
+				return false;
+		return true;
+	}
+}
